0988cc8806e7173348df775f03ccbfde31adbca3
ZoeyNight1 <zoezoe3452005@gmail.com>
2025-04-13 01:47:03 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@ import datetime
 def calculate_full_time(max_stamina, current_stamina):
     missing = max_stamina - current_stamina
     minutes_to_full = missing * 8
-    now = datetime.datetime.now()
+    now = datetime.datetime.utcnow() + datetime.timedelta(hours=8)  
     full_time = now + datetime.timedelta(minutes=minutes_to_full)
     return missing, full_time
